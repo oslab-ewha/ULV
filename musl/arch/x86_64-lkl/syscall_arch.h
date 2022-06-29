9fd4c03e99269e0869d888b661bb6c72c85cb0e6
KyungWoon Cho <cezanne@codemayo.com>
2022-06-29 18:33:26 +0900

    revert musl original system call
    Commit 9a758dc which redirects to LKL system call is dropped. The
    current seccomp implementation does not need redirection.

--- musl/arch/x86_64-lkl/syscall_arch.h
+++ musl/arch/x86_64-lkl/syscall_arch.h
@@ -1,54 +1,64 @@
 #define __SYSCALL_LL_E(x) (x)
 #define __SYSCALL_LL_O(x) (x)
 
-long lkl_syscall(long, long *);
-
 static __inline long __syscall0(long n)
 {
-	long	lkl_params[6];
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n) : "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall1(long n, long a1)
+static __inline long __syscall1(long n, long a1)
 {
-	long	lkl_params[6] = { a1 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall2(long n, long a1, long a2)
+static __inline long __syscall2(long n, long a1, long a2)
 {
-	long	lkl_params[6] = { a1, a2 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2)
+						  : "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall3(long n, long a1, long a2, long a3)
+static __inline long __syscall3(long n, long a1, long a2, long a3)
 {
-	long	lkl_params[6] = { a1, a2, a3 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3) : "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall4(long n, long a1, long a2, long a3, long a4)
+static __inline long __syscall4(long n, long a1, long a2, long a3, long a4)
 {
-	long	lkl_params[6] = { a1, a2, a3, a4 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10): "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall5(long n, long a1, long a2, long a3, long a4, long a5)
+static __inline long __syscall5(long n, long a1, long a2, long a3, long a4, long a5)
 {
-	long	lkl_params[6] = { a1, a2, a3, a4, a5 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10), "r"(r8) : "rcx", "r11", "memory");
+	return ret;
 }
 
-static __inline long
-__syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
+static __inline long __syscall6(long n, long a1, long a2, long a3, long a4, long a5, long a6)
 {
-	long	lkl_params[6] = { a1, a2, a3, a4, a5, a6 };
-	return lkl_syscall(n, lkl_params);
+	unsigned long ret;
+	register long r10 __asm__("r10") = a4;
+	register long r8 __asm__("r8") = a5;
+	register long r9 __asm__("r9") = a6;
+	__asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1), "S"(a2),
+						  "d"(a3), "r"(r10), "r"(r8), "r"(r9) : "rcx", "r11", "memory");
+	return ret;
 }
 
 #define VDSO_USEFUL
