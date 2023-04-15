d58aa193f40a0c0b071e41f93976d242f7a12993
KyungWoon Cho <cezanne@codemayo.com>
2023-04-15 20:41:15 +0900

    add blksize param for mb_file_rw

--- micro_benchmarks/file_rw_test.c
+++ micro_benchmarks/file_rw_test.c
@@ -7,29 +7,35 @@
 
 #include "libmb.h"
 
+#define BLKSIZE_MAX	16384
+
+static int	count = 1024;
+static int	blksize = 1024;
+
 static void
 usage(void)
 {
 	printf(
-"mb_rw_file <file path> [<count>]\n"
+"mb_rw_file <file path> [<count>] [<block size>]\n"
 "  default count: 1024\n"
+"  default block size: 1024, max: 16384\n"
 		);
 }
 
 static void
-write_file(int fd, int count)
+write_file(int fd)
 {
-	char	buf[1024];
+	char	buf[BLKSIZE_MAX];
 	int	i;
 
-	for (i = 0; i < 1024; i++) {
+	for (i = 0; i < blksize; i++) {
 		buf[i] = (char)i;
 	}
 	for (i = 0; i < count; i++) {
 		int	ret;
 
-		ret = write(fd, buf, 1024);
-		if (ret != 1024) {
+		ret = write(fd, buf, blksize);
+		if (ret != blksize) {
 			printf("write error\n");
 			exit(1);
 		}
@@ -37,21 +43,21 @@ write_file(int fd, int count)
 }
 
 static void
-read_file(int fd, int count)
+read_file(int fd)
 {
-	char	buf[1024];
+	char	buf[BLKSIZE_MAX];
 	int	i;
 
 	for (i = 0; i < count; i++) {
 		int	ret;
 
-		ret = read(fd, buf, 1024);
-		if (ret != 1024) {
+		ret = read(fd, buf, blksize);
+		if (ret != blksize) {
 			printf("read error\n");
 			exit(1);
 		}
 	}
-	for (i = 0; i < 1024; i++) {
+	for (i = 0; i < blksize; i++) {
 		if (buf[i] != (char)i) {
 			printf("read check failed\n");
 			exit(1);
@@ -63,14 +69,22 @@ int
 main(int argc, char *argv[])
 {
 	int	nread;
-	int	fd, count;
+	int	fd;
 
-	if (argc < 3) {
+	if (argc < 2) {
 		usage();
 		return 1;
 	}
 
-	count = atoi(argv[2]);
+	if (argc > 2)
+		count = atoi(argv[2]);
+	if (argc > 3) {
+		blksize = atoi(argv[3]);
+		if (blksize <= 0 || blksize > 16384) {
+			printf("invalid blksize\n");
+			return 1;
+		}
+	}
 
 	init_tickcount();
 
@@ -80,7 +94,7 @@ main(int argc, char *argv[])
 		return 1;
 	}
 
-	write_file(fd, count);
+	write_file(fd);
 	close(fd);
 
 	fd = open(argv[1], O_RDONLY);
@@ -88,7 +102,7 @@ main(int argc, char *argv[])
 		printf("failed to open\n");
 		return 1;
 	}
-	read_file(fd, count);
+	read_file(fd);
 	close(fd);
 
 	printf("elapsed: %d\n", get_tickcount());
