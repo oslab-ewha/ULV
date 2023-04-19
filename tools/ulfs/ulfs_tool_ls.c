5bd5e8bc3cdf12173659063db62950a158acde9f
KyungWoon Cho <cezanne@codemayo.com>
2023-04-19 21:25:37 +0900

    ulfs ls can optinally show size and inode number

--- tools/ulfs/ulfs_tool_ls.c
+++ tools/ulfs/ulfs_tool_ls.c
@@ -7,6 +7,8 @@
 #include "ulfs_tool.h"
 
 static char	typechars[] = { 'f', 'd' };
+static bool_t	ls_size = FALSE;
+static bool_t	ls_inode = FALSE;
 
 static int
 do_ls(const char *_path)
@@ -23,9 +25,15 @@ do_ls(const char *_path)
 
 	while ((ent = ulfs_dir_get(&dir))) {
 		inode_t	*inode;
+		uint32_t	ino;
 
-		inode = ulfs_get_inode(ent->bid_ib, ent->idx_ib, NULL);
-		printf("%c %s %lu\n", typechars[inode->type - 1], ent->name, inode->size);
+		inode = ulfs_get_inode(ent->bid_ib, ent->idx_ib, &ino);
+		printf("%c %s", typechars[inode->type - 1], ent->name);
+		if (ls_size)
+			printf(" %lu", inode->size);
+		if (ls_inode)
+			printf(" %u", ino);
+		printf("\n");
 	}
 
 	return 0;
@@ -38,6 +46,26 @@ ulfs_tool_ls(int argc, char *argv[])
 
 	if (argc < 1)
 		return do_ls("/");
-	
+	if (*argv[0] == '-') {
+		int	i;
+
+		for (i = 1; argv[0][i]; i++) {
+			switch (argv[0][i]) {
+			case 's':
+				ls_size = TRUE;
+				break;
+			case 'i':
+				ls_inode = TRUE;
+				break;
+			default:
+				error("%c: unknown option", argv[0][i]);
+				break;
+			}
+		}
+
+		if (argc < 2)
+			return do_ls("/");
+		return do_ls(argv[1]);
+	}
 	return do_ls(argv[0]);
 }
