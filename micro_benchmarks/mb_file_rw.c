969253b1a9bee8092ef46089dd3a85f31e53328e
KyungWoon Cho <cezanne@codemayo.com>
2023-05-14 14:39:58 +0900

    argument bug fix for mb_file_rw

--- micro_benchmarks/mb_file_rw.c
+++ micro_benchmarks/mb_file_rw.c
@@ -11,15 +11,16 @@
 #define TEST_RWFILE	"/testfile"
 #define BLKSIZE_MAX	16384
 
+static int	count_file = 1;
+static int	blksize = 1024;
 static int	count_rw = 1024;
 static int	count_loop = 1;
-static int	blksize = 1024;
 
 static void
 usage(void)
 {
 	printf(
-"mb_file_rw [<block size>] [<count rw>] [<count loop>]\n"
+"mb_file_rw [<count file>] [<block size>] [<count rw>] [<count loop>]\n"
 "  default block size: 1024, max: 16384\n"
 "  default count rw: 1024\n"
 "  default count loop: 1\n"
@@ -82,45 +83,64 @@ int
 main(int argc, char *argv[])
 {
 	int	fd;
+	int	i;
 
 	if (argc > 1) {
 		if (strcmp(argv[1], "-h") == 0) {
 			usage();
 			return 0;
 		}
-		blksize = atoi(argv[1]);
+		count_file = atoi(argv[1]);
+	}
+	if (argc > 2) {
+		blksize = atoi(argv[2]);
 		if (blksize <= 0 || blksize > 16384) {
 			printf("invalid blksize\n");
 			return 1;
 		}
 	}
-	if (argc > 2)
-		count_rw = atoi(argv[2]);
 	if (argc > 3)
-		count_loop = atoi(argv[3]);
+		count_rw = atoi(argv[3]);
+	if (argc > 4)
+		count_loop = atoi(argv[4]);
 
 	init_tickcount();
 
-	fd = open(TEST_RWFILE, O_RDWR | O_CREAT, 0660);
-	if (fd < 0) {
-		printf("failed to create\n");
-		return 1;
+	for (i = 0; i < count_file; i++) {
+		char	fpath[128];
+
+		sprintf(fpath, "%s.%d", TEST_RWFILE, i);
+		fd = open(fpath, O_RDWR | O_CREAT, 0660);
+		if (fd < 0) {
+			printf("failed to create\n");
+			return 1;
+		}
+
+		write_file(fd);
+		close(fd);
 	}
 
-	write_file(fd);
-	close(fd);
+	for (i = 0; i < count_file; i++) {
+		char	fpath[128];
 
-	fd = open(TEST_RWFILE, O_RDONLY);
-	if (fd < 0) {
-		printf("failed to open\n");
-		return 1;
+		sprintf(fpath, "%s.%d", TEST_RWFILE, i);
+		fd = open(fpath, O_RDONLY);
+		if (fd < 0) {
+			printf("failed to open\n");
+			return 1;
+		}
+		read_file(fd);
+		close(fd);
 	}
-	read_file(fd);
-	close(fd);
 
 	printf("elapsed: %d\n", get_tickcount());
 
-	unlink(TEST_RWFILE);
+	for (i = 0; i < count_file; i++) {
+		char	fpath[128];
+
+		sprintf(fpath, "%s.%d", TEST_RWFILE, i);
+		unlink(fpath);
+	}
 
 	return 0;
 }
